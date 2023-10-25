1167b3a81d1f4a28b41328fee71c5b6638658515
Mamdhooh Mohamed <mandhu305@gmail.com>
2023-10-25 14:28:51 +0500

    fix select invalid

--- src/lib/components/select/use-select.ts
+++ src/lib/components/select/use-select.ts
@@ -256,7 +256,7 @@ export function useSelect<T extends object>(originalProps: UseSelectProps<T>) {
 
   const hasHelper = !!description || !!errorMessage;
   const hasPlaceholder = !!placeholder;
-  const isInvalid = props.validationState === "invalid";
+  const isInvalid = props.validationState === "invalid" || originalProps.isInvalid;
   const shouldLabelBeOutside =
     labelPlacement === "outside-left" || (labelPlacement === "outside" && hasPlaceholder);
   const shouldLabelBeInside = labelPlacement === "inside";
